6c924821fd9637b42bac34669383b61c0bbd44b2
hj01857655 <1292548381@qq.com>
2022-10-14 20:41:43 +0800

    Update disableMonitor.js

--- disableMonitor.js
+++ disableMonitor.js
@@ -1,17 +1,17 @@
 unsafeWindow.appendChild = unsafeWindow.Element.prototype.appendChild;
-        unsafeWindow.Element.prototype.appendChild = function () {
-            try {
-                if (arguments[0].src.indexOf('detect.chaoxing.com') != -1) {
-                    return;
-                }
-            } catch (e) { }
-            unsafeWindow.appendChild.apply(this, arguments);
-        };
-        unsafeWindow.aalert = unsafeWindow.alert;
-    unsafeWindow.alert = (msg) => {
-        if (msg == '保存成功') {
+unsafeWindow.Element.prototype.appendChild = function () {
+    try {
+        if (arguments[0].src.indexOf('detect.chaoxing.com') != -1) {
             return;
         }
-        aalert(msg);
+    } catch (e) { }
+    unsafeWindow.appendChild.apply(this, arguments);
+};
+unsafeWindow.aalert = unsafeWindow.alert;
+unsafeWindow.alert = (msg) => {
+    if (msg == '保存成功') {
+        return;
     }
+    aalert(msg);
+}
     
